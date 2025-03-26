d1c6f630e15e68134f13e579e3973433fcde12f8
Weronika Kowalkowska <wkow19@gmail.com>
2025-03-26 15:05:05 +0100

    start

---
+++ zadanie1/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def encoding():
+    return 0
+def decoding():
+    return 0
+def verify_integrity():
+    return 0
+
+H_matrix=np.array([[1,1,1],[1,1,1],[1,1,1]])
+text= input("Set text to encode: ")
+
