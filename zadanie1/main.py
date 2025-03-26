349522bd31e67c13ccc5fe62941e596768ec0b0d
Weronika Kowalkowska <wkow19@gmail.com>
2025-03-26 15:24:30 +0100

    dzielne studentki pilnie pracują (jaki kolor kontórówki bedzie w modzie tej wiosny?)

--- zadanie1/main.py
+++ zadanie1/main.py
@@ -1,12 +1,30 @@
 import numpy as np
 
-def encoding():
-    return 0
-def decoding():
-    return 0
+def encoding(message):
+    wynik = np.array()
+    for i in range(message.size()):
+        wynik[i] = bytes(message[i], encoding='utf-8') #zamiana tektu na tablice bajtów
+
+    return wynik
+def decoding(encoded_message):
+    encoded_text=0
+    for i in range(encoded_message.size()):
+        encoded_text+=bytes(encoded_message[i], encoding='utf-8')
+    return encoded_text
 def verify_integrity():
     return 0
 
-H_matrix=np.array([[1,1,1],[1,1,1],[1,1,1]])
+H_matrix=np.array([
+    [1,1,1,1,0,0,0,0,1,0,0,0,0,0,0,0],
+    [1,1,0,0,1,1,0,0,0,1,0,0,0,0,0,0],
+    [1,0,1,0,1,0,1,0,0,0,1,0,0,0,0,0],
+    [0,1,0,1,0,1,1,0,0,0,0,1,0,0,0,0],
+    [1,1,1,0,1,0,0,1,0,0,0,0,1,0,0,0],
+    [1,0,0,1,0,1,0,1,0,0,0,0,0,1,0,0],
+    [0,1,1,1,1,0,1,1,0,0,0,0,0,0,1,0],
+    [1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,1]
+],dtype=int)
+
+
 text= input("Set text to encode: ")
 
