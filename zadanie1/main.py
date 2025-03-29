ddd1ca6168d3eed363f455c732a80cd9a8a8ce9e
Weronika Kowalkowska <wkow19@gmail.com>
2025-03-29 17:38:09 +0100

    proby dalej encoding

--- zadanie1/main.py
+++ zadanie1/main.py
@@ -1,17 +1,41 @@
+from email import message
+
 import numpy as np
 
 def encoding(message):
     wynik = np.array()
     for i in range(message.size()):
         wynik[i] = bytes(message[i], encoding='utf-8') #zamiana tektu na tablice bajtów
+        #znajdź bity parzyste
+        if (pow(2,i))%2==0:
+            #check_enen_bits(i)
+        else:
+
 
     return wynik
+
+'''def check_enen_bits(position, message):
+
+    index=0
+    while index<len(message):
+        for i in range (position-1):
+            index+=1
+        for i in range (position):
+            #sprawdź parzystość
+
+
+
+    return 0'''
+
 def decoding(encoded_message):
     encoded_text=0
     for i in range(encoded_message.size()):
         encoded_text+=bytes(encoded_message[i], encoding='utf-8')
     return encoded_text
-def verify_integrity():
+
+def verify_integrity(encoded_message, H_matrix):
+    for i in range(message.size()):
+        syndrome=(H_matrix*)
     return 0
 
 H_matrix=np.array([
